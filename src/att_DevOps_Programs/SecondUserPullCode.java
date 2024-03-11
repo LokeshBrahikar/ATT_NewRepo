20a1c31f30e0a0ead57e70eaacf11b089af1f98c
Lokesh Brahikar <lokeshb8855@gmail.com>
2024-03-11 23:38:20 +0530

    Adding second user code

---
+++ src/att_DevOps_Programs/SecondUserPullCode.java
@@ -0,0 +1,9 @@
+package att_DevOps_Programs;
+
+public class SecondUserPullCode 
+{
+	public static void main(String...args)
+	{
+		System.out.println("Second User Pull the Code to their system");
+	}
+}
